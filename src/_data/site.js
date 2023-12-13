d6a885dbba6a572b9af7a53f4b3d391920694899
Reza Rezaei <0xfunda@gmail.com>
2023-12-14 00:22:27 +0330

    fix site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'TayaSwap',
-  description: 'TayaSwap - blog',
+  description: 'Decntrlized Exchange and D-A-A-S',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
   url: 'https://tayaswap.xyz', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
